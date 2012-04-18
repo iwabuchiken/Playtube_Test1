b0737313fa923629e87ab8364315a091bb2dcf40
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-04-18 17:52:02 +0900

    Worked!
    1. the line is: 'android:id="@id/android:list"'
    
    [N]
    1. Create a branch ("b1")
    2. In b1, test '@android:id/list'

--- src/test/listview/Playtube_Test1Activity.java
+++ src/test/listview/Playtube_Test1Activity.java
@@ -6,6 +6,8 @@ import java.util.List;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class Playtube_Test1Activity extends ListActivity {
 	
@@ -18,6 +20,24 @@ public class Playtube_Test1Activity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        // clear the list
+        sampleList.clear();
+		
+        for (int i = 0; i < 5; i++) {
+			sampleList.add("item=" + i);
+		}//for (int i = 0; i < array.length; i++)
+        
+        // Define adapter
+        ArrayAdapter<String> adpviewList = new ArrayAdapter<String>(
+        		this,
+        		R.layout.col,
+        		sampleList);
+        
+        // set adapter
+        this.setListAdapter(adpviewList);
+        
+        // toast
+		Toast.makeText(this, "cleared", Toast.LENGTH_SHORT).show();
         
     }//public void onCreate(Bundle savedInstanceState)
 }
